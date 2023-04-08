1f2b7cc8037722a00a1531a834dc597674ba0780
风轻云断 <156348933@qq.com>
2023-04-08 22:11:55 +0800

    去掉数据库名称

--- Plugs/epay/增加支付方式.sql
+++ Plugs/epay/增加支付方式.sql
@@ -1,14 +1,14 @@
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('TRX', 0, 'TRX', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('USDT_TRC20', 0, 'USDT-TRC20', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('TRX', 0, 'TRX', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('USDT_TRC20', 0, 'USDT-TRC20', 1);
 
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_ETH', 0, 'ETH', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_USDT_ERC20', 0, 'USDT-ERC20', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_USDC_ERC20', 0, 'USDC-ERC20', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_ETH', 0, 'ETH', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_USDT_ERC20', 0, 'USDT-ERC20', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_ETH_USDC_ERC20', 0, 'USDC-ERC20', 1);
 
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_BNB', 0, 'BNB', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_USDT_BEP20', 0, 'USDT-BEP20', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_USDC_BEP20', 0, 'USDC-BEP20', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_BNB', 0, 'BNB', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_USDT_BEP20', 0, 'USDT-BEP20', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_BSC_USDC_BEP20', 0, 'USDC-BEP20', 1);
 
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_MATIC', 0, 'MATIC', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_USDT_ERC20', 0, 'USDT-Polygon', 1);
-INSERT INTO `epay`.`pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_USDC_ERC20', 0, 'USDC-Polygon', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_MATIC', 0, 'MATIC', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_USDT_ERC20', 0, 'USDT-Polygon', 1);
+INSERT INTO `pay_type` ( `name`, `device`, `showname`, `status`) VALUES ('EVM_Polygon_USDC_ERC20', 0, 'USDC-Polygon', 1);
